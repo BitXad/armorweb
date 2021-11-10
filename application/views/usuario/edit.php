c4c6168d8fbf8764ace472df9f3a2e6e5a04a0ce
douglas quisbert <duglas3093@gmail.com>
2021-11-10 17:22:54 -0400

    10.11.2021 usuario

--- application/views/usuario/edit.php
+++ application/views/usuario/edit.php
@@ -8,7 +8,7 @@
 			<div class="box-body">
 				<div class="row clearfix">
 					<div class="col-md-6">
-						<label for="usuario_nombre" class="control-label">Usuario Nombre</label>
+						<label for="usuario_nombre" class="control-label">Nombre</label>
 						<div class="form-group">
 							<input type="text" name="usuario_nombre" value="<?php echo ($this->input->post('usuario_nombre') ? $this->input->post('usuario_nombre') : $usuario['usuario_nombre']); ?>" class="form-control" id="usuario_nombre" />
 						</div>
@@ -19,12 +19,19 @@
 							<input type="text" name="usuario_login" value="<?php echo ($this->input->post('usuario_login') ? $this->input->post('usuario_login') : $usuario['usuario_login']); ?>" class="form-control" id="usuario_login" />
 						</div>
 					</div>
+					<div class="col-md-6">
+						<label for="usuario_clave" class="control-label">Usuario clave</label>
+						<div class="form-group">
+							<input type="password" name="usuario_clave" value="<?php echo ($this->input->post('usuario_clave') ? $this->input->post('usuario_clave') : $usuario['usuario_login']); ?>" class="form-control" id="usuario_login" />
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="box-footer">
             	<button type="submit" class="btn btn-success">
-					<i class="fa fa-check"></i> Save
+					<i class="fa fa-check"></i> Guardar
 				</button>
+				<a href="<?php echo site_url('usuario'); ?>" class="btn btn-danger"><i class="fa fa-times"></i> Cancelar</a>
 	        </div>				
 			<?php echo form_close(); ?>
 		</div>
